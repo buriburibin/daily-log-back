2c48f87e3267f6f0f7038b6213904d34dfb41322
SungBin <djsbpark@naver.com>
2023-04-04 16:45:10 +0900

    * setter 추가

--- src/main/java/com/daily/log/userInfo/UserInfoRequestDto.java
+++ src/main/java/com/daily/log/userInfo/UserInfoRequestDto.java
@@ -7,9 +7,11 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserInfoRequestDto {
